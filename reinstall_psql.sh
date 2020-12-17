5ec9f1f3d5e8054d82ea6b81988657a3bb3ed7b9
stach <staho.kazmierowski@gmail.com>
2020-12-17 13:31:22 +0000

    reinstalling psql script

---
+++ reinstall_psql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get --purge remove postgresql-*
+sudo rm -Rf /etc/postgresql /var/lib/postgresql
+sudo apt-get install postgresql
+sudo su - postgres -c "createuser -s stach"
+sudo su - postgres -c "createdb stach"
+psql -c "CREATE EXTENSION file_fdw;"
+psql -c "CREATE SERVER csv FOREIGN DATA WRAPPER file_fdw;"
