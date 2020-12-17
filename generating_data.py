4958aa1104e2644ebd66572a8bfa2a71915aa913
Stach <staho.kazmierowski@gmail.com>
2020-12-17 14:13:12 +0100

    initial commit

---
+++ generating_data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+MEAN = 1000
+VAR = 150
+LOW = 500
+HIGH = 1500
+
+SWEETS = ["'Milka'", "'Mieszanka studencka'", "'Lindor'", "'Prince polo'", "'Delicje'", "'Kinder niespodzianka'"
+    , "'Jeżyki'", "'Ferrero Rocher'", "'Merci'", "'Raphaello'"]
+
+def gen_similarity(SWEETS):
+    sweets_size = len(SWEETS)
+    similarity = np.random.uniform(0, 1, size=(sweets_size, sweets_size)).round(decimals=2)
+    data = pd.DataFrame()
+    for i in range(sweets_size):
+        for j in range(i):
+            data = data.append(pd.DataFrame([[SWEETS[i], SWEETS[j], similarity[i,j]]]))
+    data.to_csv('./data/similarity.csv', index=False, header=False)
+    return data
+
+def gen_sweets(SWEETS):
+    sweets_amount = np.random.uniform(LOW, HIGH, size=(len(SWEETS), 1)).astype(int)
+    df0 = pd.DataFrame(SWEETS)
+    df1 = pd.DataFrame(sweets_amount)
+    df = pd.concat([df0,df1], axis=1)
+    df.to_csv('./data/sweets.csv', index=False, header=False)
+    # print(df)
+
+
+print(gen_similarity(SWEETS))
+gen_sweets(SWEETS)
---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+SWEETS = ['Milka', 'Mieszanka studencka', 'Lindor', 'Prince polo', 'Delicje', 'Kinder niespodzianka', 'Jeżyki', 'Ferrero Rocher', 'Merci', 'Raphaello']
+
